00d80fd2bb56d87cd8a194605d0d2de788c5538d
Joel Chuco <63567617+joelchucomarrufo@users.noreply.github.com>
2024-02-07 09:06:28 -0500

    change code response

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/iniciarSesion', (req, res) => {
     // Find user in the mock database
     const user = users.find(user => user.username === username && user.password === password);
     if (!user) {
-        return res.status(401).json({ error: 'Invalid username or password' });
+        return res.json({ error: 'Invalid username or password' }); 
     }
     res.json({ message: 'Login successful' });
 });
